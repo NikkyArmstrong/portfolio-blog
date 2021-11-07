1634efb014aa330bcb3a1662fb7a9461bb11479a
nikkydarmstrong <armstrong.nikky@gmail.com>
2021-11-07 14:35:20 +0000

    use flex for header on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -27,28 +27,26 @@ const Header = () => {
     <header className={headerStyles.header}>
       <div className={headerStyles.accent}></div>
       <section className={headerStyles.sideBarContainer}>
-      <div className={headerStyles.heroContent}>
-        <p className={headerStyles.brand}>
-          <Link to="/">{data.site.siteMetadata.title}</Link>
-        </p>
-        <p className={headerStyles.description}>{data.site.siteMetadata.description}</p>
-      </div>
-      <div className={headerStyles.socialContainer}>
+        <div className={headerStyles.heroContent}>
+          <p className={headerStyles.brand}>
+            <Link to="/">{data.site.siteMetadata.title}</Link>
+          </p>
+          <p className={headerStyles.description}>{data.site.siteMetadata.description}</p>
+        </div>
         <Socials />
-      </div>
-      <nav className={headerStyles.navContainer}>
-        <ul className={headerStyles.navList}>
-          {data.site.siteMetadata.header.nav_links.map(link => {
-            return (
-              <li key={link.label}>
-                <Link to={link.url}>
-                  {link.label}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
+        <nav className={headerStyles.navContainer}>
+          <ul className={headerStyles.navList}>
+            {data.site.siteMetadata.header.nav_links.map(link => {
+              return (
+                <li key={link.label}>
+                  <Link to={link.url}>
+                    {link.label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
       </section>
     </header>
   )
