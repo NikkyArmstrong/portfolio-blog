c3e602d3e0827339f8d5c1d276f7af1951d44905
nikkydarmstrong <armstrong.nikky@gmail.com>
2022-01-16 22:46:18 +0000

    tidy up links for commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,10 +36,11 @@ export default function Index({location}) {
       <h1>Nikky Armstrong</h1>
       <p>Hello! I am a Software Engineer with over ten years' experience across vastly different industries, including web development, mining simulation, and both AAA & Indie game development.</p>
       <GatsbyImage class={layoutStyles.headshot} alt="Nikky with her dog, Growlithe" placeholder="blurred" image={image} />
-      <p>I am currently Lead Programmer at <OutboundLink href="https://silverraingames.com">Silver Rain Games</OutboundLink> where as well as writing tools & gameplay code, I make sure the code team is happy and healthy, with a valid MOT.</p>
+      <p>I am currently Lead Programmer at <OutboundLink href="https://silverraingames.com">Silver Rain Games</OutboundLink> where as well as writing tools & gameplay code, I make sure the code team is ticking along happily.</p>
       <p>Before 2020, I lived and worked in Adelaide and then Perth, Australia. In 2019 I was offered a position as Senior Gameplay Programmer with Ubisoft Reflections, and so on March 10th 2020 I landed in the UK, excited to spend the next few years exploring everything my new home on the border of Northumberland, the wider UK, and the Northern Hemisphere had to offer. I quickly discovered that the inside of my flat was quite nice, thank you.</p>
       <p>I am passionate about mentoring and teaching, improving the experience for people in underrepresented groups in the tech & games industries, and bringing concepts and workflows from software development into the games industry. I am an engaging speaker who is constantly talking to anyone who will listen about all of these topics, and would love to be invited to do this at your event or workplace.</p>
       <p>When I am not at work or banging on about automated tests, I can probably be found rambling in a muddy field with my dog, Growlithe.</p>
+      <p>If you would like to contact me, please feel free to do so on <OutboundLink href="https://twitter.com/nikkyarmstrong">Twitter</OutboundLink> or <OutboundLink href="https://www.linkedin.com/in/nikky-armstrong/">LinkedIn</OutboundLink>.</p>
     </Layout>
   )
 }
