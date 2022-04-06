988a66c6bf97cce05fa577ebca088d9ae749bb81
Nikky Armstrong <narmstrong@silverraingames.com>
2022-04-06 20:18:17 +0100

    Update speaking engagements

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -20,7 +20,7 @@ export default function Experience({location}) {
       <h2>Experience</h2>
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Lead Programmer</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://silverraingames.com" target="_blank" rel="noreferrer">Silver Rain Games</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://silverraingames.com" target="_blank" rel="noreferrer">Silver Rain Games</OutboundLink></span>
         <span className={experienceStyles.year}>2021-</span>
         <span className={experienceStyles.extraInfo}>
           I was brought on as Lead Programmer for Silver Rain's existing programming team of three developers, who had until then been operating without a Lead. My initial steps were to instate a formal code review process, review the coding standards, start a fortnightly knowledge share meeting, and add sprint retrospectives to our agile process. I also set to work planning a full project backlog for the programming team representing all currently known work yet to be completed for the team, and assigning that work to approximate milestones. I formalised the daily standups, ensuring the team was keeping task tracking up to date so I could assist with scheduling work and setting expectations with the rest of the studio about what we can realistically deliver. I have worked with my team to ensure that we operate internally and with other disciplines respectfully, and that we are always growing together to improve our coding skills. Code-wise, I have made expansive changes to our internal tools so they meet the needs of the rest of the studio, and contributed to gameplay and engine improvements.
@@ -33,7 +33,7 @@ export default function Experience({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Senior Gameplay Programmer</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://reflections.ubisoft.com" target="_blank" rel="noreferrer">Ubisoft Reflections</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://reflections.ubisoft.com" target="_blank" rel="noreferrer">Ubisoft Reflections</OutboundLink></span>
         <span className={experienceStyles.year}>2020-2021</span>
         <span className={experienceStyles.extraInfo}>
           At Ubisoft I was a Senior member of the AI Team owning the ambient dialogue system, all pre-combat AI behaviours, and was main point of contact for the navmesh and navigation tech. I mentored and supported more junior members of the team, helping them learn programming, AI, and Agile concepts. While at Ubisoft, I founded a cross-discipline C++ Discussion Group to ensure all programmers of all levels could help each other to learn and grow, as well as a networking group for women on the gameplay programming teams.
@@ -46,7 +46,7 @@ export default function Experience({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Software Engineer IV</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.immersivetechnologies.com/" target="_blank" rel="noreferrer">Immersive Technologies</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.immersivetechnologies.com/" target="_blank" rel="noreferrer">Immersive Technologies</OutboundLink></span>
         <span className={experienceStyles.year}>2016-2020</span>
         <span className={experienceStyles.extraInfo}>
           While only employed as an Intermediate level programmer, I planned and lead multiple projects from end to end, including gathering requirements, creating and estimating the project backlog, planning sprints, and communicating and demong the project progress to internal and external stakeholders. As well as project planning I was responsible for a number of interesting technical solutions. These include the AI for our new simulation for Komatsu's Autonomous Haul Trucks, optimising our simulators' startup cycle from cold boot of the hardware through to beginning the training program, and implementation of the "Immersive Package Manager", software responsible for rolling our live updates of our products to the customers' simulators. Throughout my time at Immersive I gained a reputation for being able to debug and fix complex issues in any of our products quickly and effectively, regardless of whether I had worked in that code before.
@@ -56,7 +56,7 @@ export default function Experience({location}) {
       <h2>Volunteering</h2>
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Mentor</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.limitbreak.co.uk/" target="_blank" rel="noreferrer">Limit Break</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.limitbreak.co.uk/" target="_blank" rel="noreferrer">Limit Break</OutboundLink></span>
         <span className={experienceStyles.year}>2020, 2021</span>
         <span className={experienceStyles.extraInfo}>
           Limit Break is a six month mentoring relationship with a person from an underrepresented group working in the UK games industry. In 2020 I was paired with a person in their final year of university wanting to pursue a career in games. We met together every 2-3 weeks for a video call, and I continued these meetings beyond the initial six months so that I could support them through the end of their degree. In 2021 I volunteered again, taking on two mentees, and I am looking forward to the 2022 intake.
@@ -69,7 +69,7 @@ export default function Experience({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Mentor</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.thegirlsnetwork.org.uk/" target="_blank" rel="noreferrer">The Girls' Network</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.thegirlsnetwork.org.uk/" target="_blank" rel="noreferrer">The Girls' Network</OutboundLink></span>
         <span className={experienceStyles.year}>2021</span>
         <span className={experienceStyles.extraInfo}>
           Through the Girls Network program I am mentoring a high school aged girl. The program aims to provide work and life skills to mentees, for example confidence, communication, personal finance, wellbeing, and to connect them with opportunities to pursue their goals.
@@ -79,7 +79,7 @@ export default function Experience({location}) {
       <h2>Education</h2>
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Bachelor of Computer Science (Hons. First class)</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.adelaide.edu.au/" target="_blank" rel="noreferrer">The University of Adelaide</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.adelaide.edu.au/" target="_blank" rel="noreferrer">The University of Adelaide</OutboundLink></span>
         <span className={experienceStyles.year}>2007 - 2010</span>
         <span className={experienceStyles.extraInfo}>
           I graduated in 2009 with a Bachelor of Computer Science (Software Engineering) with a minor in pure mathematics. In 2010 I graduated with First Class Honours, having completed a research thesis considering the application of unit tests and formal verification to video games.
--- src/pages/speaking.js
+++ src/pages/speaking.js
@@ -15,7 +15,7 @@ export default function Speaking({location}) {
                 pathname={location.pathname} />
       <h1> </h1>
       <p>I am always looking for opportunities to encourage and empower people from traditionally underrepresented groups into jobs in tech and games. As well as my involvement with mentoring programs, which can be found on my <Link href="/experience">Experience</Link> page, this page lists details of events I have been invited to be a part of.</p>
-      <p>I enjoy public speaking, and my first talk in the UK games industry at Develop:Brighton 2021 received fantastic feedback. I have been invited to give this talk again at an internal event for a UK based studio, and am hoping to submit talks to further conferences in 2022.</p>
+      <p>I enjoy public speaking, and my first talk in the UK games industry at Develop:Brighton 2021 received fantastic feedback. As a result, I was invited to give this talk again at an internal event for a large UK based studio. I have been invited back to Develop in 2022, and have a couple more talks lined up this year as well.</p>
       <p>If you would like to talk to me about your event, feel free to get in touch via <OutboundLink href="https://twitter.com/nikkyarmstrong" target="_blank" rel="noreferrer">Twitter</OutboundLink>, <OutboundLink href="https://www.linkedin.com/in/nikky-armstrong/" target="_blank" rel="noreferrer">LinkedIn</OutboundLink>, or my <Link to="/contact">Contact page</Link>.</p>
 
       <h2>Talks</h2>
@@ -24,8 +24,8 @@ export default function Speaking({location}) {
         <span className={experienceStyles.title}>
           Develop:Brighton 2021
         </span>
-        <span className={experienceStyles.company}>
-          <OutboundLink href="https://www.developconference.com/whats-on/speakers/speaker-detail/nikky-armstrong" target="_blank" rel="noreferrer">
+        <span className={experienceStyles.talklink}>
+          <OutboundLink href="https://www.developconference.com/" target="_blank" rel="noreferrer">
           Test Driven Development: A Mindset to Develop Games from the Start, Middle, or End
           </OutboundLink>
         </span>
@@ -42,7 +42,7 @@ export default function Speaking({location}) {
         <span className={experienceStyles.title}>
           Sega Dev Day, March 2022
         </span>
-        <span className={experienceStyles.company}>
+        <span className={experienceStyles.talklink}>
           <OutboundLink href="https://twitter.com/CAGames/status/1499408212650512389?s=20&t=X8Wi0ZUjZjTkWEF5kI5u9g" target="_blank" rel="noreferrer">
           TDD as a Mindset: Make Test-First Development a Reality
           </OutboundLink>
@@ -56,7 +56,7 @@ export default function Speaking({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>She Plays Games Podcast</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.sheplaysgames.co/episodes/nikky-armstrong-michelle-snowball" target="_blank" rel="noreferrer">
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.sheplaysgames.co/episodes/nikky-armstrong-michelle-snowball" target="_blank" rel="noreferrer">
           Podcast Episode #47
         </OutboundLink></span>
         <span className={experienceStyles.extraInfo}>
@@ -70,7 +70,7 @@ export default function Speaking({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>BAFTA Young Game Designers</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://www.youtube.com/watch?v=yxXjAcrZigo" target="_blank" rel="noreferrer">
+        <span className={experienceStyles.talklink}><OutboundLink href="https://www.youtube.com/watch?v=yxXjAcrZigo" target="_blank" rel="noreferrer">
           YGD Masterclass: Programming
         </OutboundLink></span>
         <span className={experienceStyles.infoWithPicture}>
@@ -85,14 +85,14 @@ export default function Speaking({location}) {
 
       <section className={experienceStyles.experienceBlock}>
         <span className={experienceStyles.title}>Careers Talks</span>
-        <span className={experienceStyles.company}><OutboundLink href="https://youtu.be/tFIRqBHg-v0?t=870" target="_blank" rel="noreferrer">Ukie</OutboundLink> & <OutboundLink href="https://youtu.be/Rh5qXr_s0Y0?t=392" target="_blank" rel="noreferrer">Games Jobs Live</OutboundLink></span>
+        <span className={experienceStyles.talklink}><OutboundLink href="https://youtu.be/tFIRqBHg-v0?t=870" target="_blank" rel="noreferrer">Ukie</OutboundLink> & <OutboundLink href="https://youtu.be/Rh5qXr_s0Y0?t=392" target="_blank" rel="noreferrer">Games Jobs Live</OutboundLink></span>
         <span className={experienceStyles.extraInfo}>Links to a couple of careers talks I have given recently. I'm always excited to speak to people considering a career in the games industry about potential pathways into programming.</span>
       </section>
 
       <h2>Upcoming</h2>
       <section className={experienceStyles.experienceBlock}>
-        <span className={experienceStyles.title}>Women in Tech Global Conference 2022</span>
-        <span className={experienceStyles.company}>
+        <span className={experienceStyles.title}>Women in Tech Global Conference, June 2022</span>
+        <span className={experienceStyles.talklink}>
           <OutboundLink href="https://www.womentech.net/speaker/Nikky/Armstrong/68943" target="_blank" rel="noreferrer">
             Software as a Service: Cultivating a respectful programming team in a multi-discipline games studio
           </OutboundLink>
@@ -102,9 +102,39 @@ export default function Speaking({location}) {
         </span>
       </section>
 
-      { /* <p>Cheltenham Science Festival</p> */}
+      <div className={experienceStyles.dividerDiv}>
+        <FontAwesomeIcon className={experienceStyles.divider} aria-label='divider' title='divider' icon={faStream} />
+      </div>
 
+      <section className={experienceStyles.experienceBlock}>
+        <span className={experienceStyles.title}>Cheltenham Science Festival, June 2022</span>
+        <span className={experienceStyles.talklink}>
+          <OutboundLink href="https://www.cheltenhamfestivals.com/science-/whats-on/2022/games-dev-live-how-to-make-a-game-/" target="_blank" rel="noreferrer">
+            Games Dev Live: How To Make A Game
+          </OutboundLink>
+          </span>
+          <span className={experienceStyles.extraInfo}>
+            Ever wondered what it takes to bring a game concept to life? Or what makes a successful game? In this dynamic event join us to see how a game idea develops live on our stage. Corey Brotherson and Nikky Armstrong from independent games development studio Silver Rain Games come together with presenter Alysia Judge to brainstorm an idea around our theme 'Be The Change'. They'll delve into why their ideas may work, how they could be best executed and the ways that the process could develop. Prepare for an exciting insight into games development.
+          </span>
+      </section>
 
+      <div className={experienceStyles.dividerDiv}>
+        <FontAwesomeIcon className={experienceStyles.divider} aria-label='divider' title='divider' icon={faStream} />
+      </div>
+
+      <section className={experienceStyles.experienceBlock}>
+        <span className={experienceStyles.title}>
+          Develop:Brighton, July 2022
+        </span>
+        <span className={experienceStyles.talklink}>
+          <OutboundLink href="https://www.developconference.com/whats-on/speakers/speaker-detail/nikky-armstrong" target="_blank" rel="noreferrer">
+          Is Your Code Working For You?
+          </OutboundLink>
+        </span>
+        <span className={experienceStyles.extraInfo}>
+          Is your code working for you, or are you working for your code? Are you writing the same code you've always written, and are worried whether it's the best way to do things? Are you following "best practice" advice, but want to understand why those are the rules? Game development is its own ball game, and there are sometimes unwritten rules about which language best practices are applicable. Sometimes a language evolves, or we switch languages, and the best practices we used to swear by now don't make sense. Sometimes we're confident we know exactly what a code snippet will do, and when we run it - it doesn't quite do what we expect. This talk will present tips and tricks for games programming, from beginner to advanced, C++, C#, and beyond. It will present the good, the bad, and the sometimes hilarious of games programming, using real examples and code snippets, and provide tools and exercises to take you beyond this talk to explore deeper into the code you write every day.
+        </span>
+      </section>
     </Layout>
   )
 }
